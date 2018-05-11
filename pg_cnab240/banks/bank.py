ed3971abdeebdd4ec051f8d55dc0d2eeabb50a19
Alamo Saravali <alamo.saravali@gmail.com>
2018-05-11 12:09:09 -0300

    added bank segments and updated get payment_segment method

--- pg_cnab240/banks/bank.py
+++ pg_cnab240/banks/bank.py
@@ -3,6 +3,14 @@ class Bank:
         self.name = name
         self.slug = slug
         self.code = code
+        self.available_segments = dict()
+    
+    def set_segment(self, segment_name, segment_class, payment_types=dict()):
+        self.available_segments[segment_name] = dict(
+            segment_name = segment_name,
+            segment_class = segment_class,
+            payment_types = payment_types,
+        )
     
     def get_file_header(self):
         pass
@@ -14,4 +22,7 @@ class Bank:
         pass
     
     def get_payment_segment(self, payment_type):
-        pass
+        for segment_name, segment_dict in self.available_segments.items():
+            if payment_type in [*segment_dict['payment_types']]:
+                return segment_dict
+        raise Exception('Payment Type not Found')
