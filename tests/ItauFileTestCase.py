71015b5e66118ff207fad92c2ef358f3c96941ae
Alamo Saravali <alamo.saravali@gmail.com>
2018-05-19 11:43:14 -0300

    ItauFileTestCase: - Updated SegmentA test; - generate a file to check backup string;

--- tests/ItauFileTestCase.py
+++ tests/ItauFileTestCase.py
@@ -3,6 +3,7 @@ from pg_cnab240.file import File
 from pg_cnab240.company import Company
 from pg_cnab240.payment import Payment
 from datetime import datetime
+import os
 
 # create company object
 company = Company('BF Servicos De Cobranca Ltda', '07179434000140')
@@ -38,9 +39,14 @@ class ItauFileTestCase(unittest.TestCase):
         payment_file.generate()
 
         body_big_line = ''.join(payment_file.body)
-        print(body_big_line)
 
-        assert '34100031C2041040 207179434000140                    00772 000000020070 5BF SERVICOS DE COBRANCA LTDA                                          Av Andromeda                  02000Bl84 andar     Barueri             06473000SP                  3410003300001A00000003303875 000013006629 0felipe lucifero rosado eventos5511972063805440    10052018REA904008880000000000000000240000                    00000000000000000000000                    00000021044237000144                       34100035         000003000000000000240000000000000000000000                                                                                                                                                                                     ' in body_big_line
+        root_path = os.path.dirname(os.path.abspath(__file__))
+        body_big_file_path = os.path.join(root_path, 'body_big_file.txt')
+        f = open(body_big_file_path, 'w')
+        f.write(body_big_line)
+        f.close()
+
+        assert '34100011C2041040 207179434000140                    03875 000013006629 3BF SERVICOS DE COBRANCA LTDA                                          AV ANDROMEDA                  02000BL84 ANDAR     BARUERI             06473000SP                  3410001300001A00000003303875 000013006629 0FELIPE LUCIFERO ROSADO EVENTOS5511972063805440    10052018REA904008880000000000000000240000                            000000000000000                    00000021044237000144                       34100015         000003000000000000240000000000000000000000                                                                                                                                                                                     ' in body_big_line
 
 
 if __name__ == '__main__':
