637335dbd49df1e204aea622a91629b2b16312ee
Alamo Saravali <alamo.saravali@gmail.com>
2018-05-21 17:54:20 -0300

    ItauFileTestCase: - finished tests for Segment ANF;

--- tests/ItauFileTestCase.py
+++ tests/ItauFileTestCase.py
@@ -26,28 +26,28 @@ class ItauFileTestCase(unittest.TestCase):
         # print(len(header_line))
         assert '240' in str(len(header_line))
     
-    # def test_2_process_one_paymentA(self):
-    #     payment_file = File('itau', company)
-    #     payment_file.header.set_company_data(company)
-    #     payment_file.payments = []
+    def test_2_process_one_paymentA(self):
+        payment_file = File('itau', company)
+        payment_file.header.set_company_data(company)
+        payment_file.payments = []
 
-    #     payment = Payment(type='ted', favored_name='Cliente Teste', favored_bank='033', agency='01111', account='000011111111', account_digit=0, your_number='5511972063805440', pay_date='10052018', ispb_code='90400888', payment_amount=2400.00, favored_document_number='11111111111111')
+        payment = Payment(type='ted', favored_name='Cliente Teste', favored_bank='033', agency='01111', account='000011111111', account_digit=0, your_number='5511972063805440', pay_date='10052018', ispb_code='90400888', payment_amount=2400.00, favored_document_number='11111111111111')
 
-    #     # add payment
-    #     payment_file.add_payment(payment)
+        # add payment
+        payment_file.add_payment(payment)
 
-    #     # generate
-    #     payment_file.generate()
+        # generate
+        payment_file.generate()
 
-    #     body_big_line = ''.join(payment_file.body)
+        body_big_line = ''.join(payment_file.body)
 
-    #     root_path = os.path.dirname(os.path.abspath(__file__))
-    #     body_big_file_path = os.path.join(root_path, 'body_big_file_segment_a.txt')
-    #     f = open(body_big_file_path, 'w')
-    #     f.write(body_big_line)
-    #     f.close()
+        root_path = os.path.dirname(os.path.abspath(__file__))
+        body_big_file_path = os.path.join(root_path, 'body_big_file_segment_a.txt')
+        f = open(body_big_file_path, 'w')
+        f.write(body_big_line)
+        f.close()
 
-    #     assert '34100011C2041040 207179434000140                    01111 000011111111 3BF SERVICOS DE COBRANCA LTDA                                          AV ANDROMEDA                  02000BL84 ANDAR     BARUERI             06473000SP                  3410001300001A00000003301111 000011111111 0CLIENTE TESTE                 5511972063805440    10052018REA904008880000000000000000240000                            000000000000000                    00000011111111111111                       34100015         000003000000000000240000000000000000000000                                                                                                                                                                                     ' in body_big_line
+        assert '34100011C2041040 207179434000140                    01111 000011111111 3BF SERVICOS DE COBRANCA LTDA                                          AV ANDROMEDA                  02000BL84 ANDAR     BARUERI             06473000SP                  3410001300001A00000003301111 000011111111 0CLIENTE TESTE                 5511972063805440    10052018REA904008880000000000000000240000                            000000000000000                    00000011111111111111                       34100015         000003000000000000240000000000000000000000                                                                                                                                                                                     ' in body_big_line
     
     def test_3_process_one_paymentJ(self):
         payment_file = File('itau', company)
@@ -72,6 +72,30 @@ class ItauFileTestCase(unittest.TestCase):
         f.close()
 
         assert '34100011C2031030 207179434000140                    01111 000011111111 3BF SERVICOS DE COBRANCA LTDA                                          AV ANDROMEDA                  02000BL84 ANDAR     BARUERI             06473000SP                  3410001300001J00003394752400010067609485239700000007661190101CLIENTE TESTE                 14052018000000001006760000000000000000000000000000000100520180000000010067600000000000000005506715023835136                                          34100015         000003000000000001006760000000000000000000                                                                                                                                                                                     ' in body_big_line
+    
+    def test_4_process_one_paymentANF(self):
+        payment_file = File('itau', company)
+        payment_file.header.set_company_data(company)
+        payment_file.payments = []
+
+        payment = Payment(type='nf', favored_name='Cliente Teste', favored_bank='033', agency='01111', account='000011111111', account_digit=0, pay_date='10052018', ispb_code='90400888', payment_amount=10067.60, dv='4', due_rule='7524', amount=10067.60, free_field='9485239700000007661190101', due_date='14052018', title_amount=10067.60, your_number='5511972063805440', nf_document='5646510065784320', favored_document_number='11111111111111')
+
+        # add payment
+        payment_file.add_payment(payment)
+
+        # generate
+        payment_file.generate()
+
+        body_big_line = None
+        body_big_line = ''.join(payment_file.body)
+
+        root_path = os.path.dirname(os.path.abspath(__file__))
+        body_big_file_path = os.path.join(root_path, 'body_big_file_segment_anf.txt')
+        f = open(body_big_file_path, 'w')
+        f.write(body_big_line)
+        f.close()
+
+        assert '34100011C2041040 207179434000140                    01111 000011111111 3BF SERVICOS DE COBRANCA LTDA                                          AV ANDROMEDA                  02000BL84 ANDAR     BARUERI             06473000SP                  3410001300001A00000003301111 000011111111 0CLIENTE TESTE                 5511972063805440    10052018REA000000000000000000000001006760                            00000000000000056465100657843      000000111111111111112                      34100015         000003000000000001006760000000000000000000                                                                                                                                                                                     ' in body_big_line
 
 
 if __name__ == '__main__':
