fc5b84789f178101ff3544d5cc9bf0fbbd4bbe3b
Guilherme <guiligan@gmail.com>
2019-07-16 18:41:57 -0300

    Simple test for A and J segments with single entry

---
+++ Tests/Itau/testItauPaymentA.py
@@ -0,0 +1,51 @@
+from .ItauUtils import company, company_j
+from pg_cnab240.banks.Itau.payment_methods import PaymentMethods
+from pg_cnab240.file import File
+from pg_cnab240.payment import Payment
+import os
+import Tests.Utils as Utils
+import unittest
+
+
+class TestItauPaymentA(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    @staticmethod
+    def test_process_one_paymentA():
+        payment_file = File('Itau', company)
+        payment_file.header.set_company_data(company)
+        payment_file.payments = []
+
+        payment = Payment(type=PaymentMethods.TED,
+                          favored_name=company_j.name,
+                          favored_bank=company_j.bank_account.bank_code,
+                          agency=company.bank_account.agency,
+                          account=company.bank_account.account,
+                          account_digit=company.bank_account.digit,
+                          your_number='n1U0fXg4Tl2RLotU0YQ8',
+                          pay_date='01012019',
+                          payment_amount=2400.00,
+                          favored_document_number=company_j.document)
+        payment_file.add_payment(payment)
+        payment_file.generate()
+        body_big_line = payment_file.get_content()
+
+        root_path = os.path.dirname(os.path.abspath(__file__))
+        body_big_file_path = os.path.join(root_path, 'body_big_file_segment_a.rem')
+
+        f = open(body_big_file_path, 'w')
+        f.write(body_big_line)
+        f.close()
+
+        file_hash = Utils.hash_file(body_big_file_path, True)
+        os.remove(body_big_file_path)
+
+        assert 'b31957cda8950105f0e659b9ed70ecc01ef1aee253e59d56d3596d6ffc6aed93' == file_hash
+
+
+if __name__ == '__main__':
+    unittest.main()
--- Tests/ItauFileTestCase.py
+++ Tests/ItauFileTestCase.py
@@ -1,8 +1,8 @@
+import os
 import unittest
 from pg_cnab240.file import File
 from pg_cnab240.company import Company
 from pg_cnab240.payment import Payment
-import os
 
 # create company object
 company = Company('BF Servicos De Cobranca Ltda', '07179434000140')
@@ -28,93 +28,7 @@ class ItauFileTestCase(unittest.TestCase):
     #     header_line = payment_file.header.to_line()
     #     assert '240' in str(len(header_line))
     #
-    # @staticmethod
-    # def test_2_process_one_paymentA():
-    #     payment_file = File('Itau', company)
-    #     payment_file.header.set_company_data(company)
-    #     payment_file.payments = []
-    #
-    #     payment = Payment(type='ted',
-    #                       favored_name='Cliente Teste',
-    #                       favored_bank='033', agency='01111',
-    #                       account='000011111111',
-    #                       account_digit=0,
-    #                       your_number='5511972063805440',
-    #                       pay_date='10052018',
-    #                       ispb_code='90400888',
-    #                       payment_amount=2400.00,
-    #                       favored_document_number='11111111111111')
-    #
-    #     # add payment
-    #     payment_file.add_payment(payment)
-    #
-    #     # generate
-    #     payment_file.generate()
-    #
-    #     body_big_line = ''.join(payment_file.body)
-    #
-    #     root_path = os.path.dirname(os.path.abspath(__file__))
-    #     body_big_file_path = os.path.join(root_path, 'body_big_file_segment_a.rem')
-    #     f = open(body_big_file_path, 'w')
-    #     f.write(body_big_line)
-    #     f.close()
-    #
-    #     assert '34100011C2041040 207179434000140                    01111 000011111111 3BF SERVICOS DE COBRANCA LTDA' \
-    #            '                                          AV ANDROMEDA                  02000BL84 ANDAR     BARUERI' \
-    #            '             06473000SP                  3410001300001A00000003301111 000011111111 0CLIENTE TESTE' \
-    #            '                 5511972063805440    10052018REA904008880000000000000000240000' \
-    #            '                            000000000000000                    00000011111111111111' \
-    #            '                       34100015         000003000000000000240000000000000000000000' \
-    #            '                                                                                                    ' \
-    #            '                                                                                 ' in body_big_line
-    #
-    @staticmethod
-    def test_3_process_one_paymentJ():
-        payment_file = File('Itau', company)
-        payment_file.header.set_company_data(company)
-        payment_file.payments = []
 
-        payment = Payment(type='other_bank_slip',
-                          favored_name=company_j.name,
-                          favored_bank=company_j.bank_account.bank_code,
-                          agency=company.bank_account.agency,
-                          account=company.bank_account.account,
-                          account_digit=company.bank_account.digit,
-                          pay_date='05072019',
-                          currency_type=9,
-                          dv='3',
-                          due_rule='7941',
-                          amount=1029.41,
-                          free_field='0296090001270557500010810',
-                          due_date='05072019',
-                          title_amount=1029.41,
-                          payment_amount=1029.41,
-                          your_number='12345678901234567890',
-                          recipient_document_type=2,
-                          recipient_document_number=20093235000182,
-                          recipient_name='ARIZONA 701')
-
-        # add payment
-        payment_file.add_payment(payment)
-
-        # generate
-        payment_file.generate()
-        body_big_line = payment_file.get_content()
-
-        root_path = os.path.dirname(os.path.abspath(__file__))
-        body_big_file_path = os.path.join(root_path, 'body_big_file_segment_j.rem')
-        f = open(body_big_file_path, 'w')
-        f.write(body_big_line)
-        f.close()
-
-        assert '34100011C2031030 207179434000140                    01111 000011111111 3BF SERVICOS DE COBRANCA LTDA' \
-               '                                          AV ANDROMEDA                  02000BL84 ANDAR     BARUERI ' \
-               '            06473000SP                  3410001300001J00003394752400010067609485239700000007661190101' \
-               'CLIENTE TESTE                 14052018000000001006760000000000000000000000000000000100520180000000010' \
-               '067600000000000000005506715023835136                                          34100015         000003' \
-               '000000000001006760000000000000000000                                                                 ' \
-               '                                                                                                     ' \
-               '               ' in body_big_line
     #
     # @staticmethod
     # def test_4_process_one_paymentANF():
@@ -153,15 +67,15 @@ class ItauFileTestCase(unittest.TestCase):
     #     f.write(body_big_line)
     #     f.close()
     #
-    #     assert '34100011C2041040 207179434000140                    01111 000011111111 3BF SERVICOS DE COBRANCA LTDA' \
-    #            '                                          AV ANDROMEDA                  02000BL84 ANDAR     BARUERI ' \
-    #            '            06473000SP                  3410001300001A00000003301111 000011111111 0CLIENTE TESTE    ' \
-    #            '             5511972063805440    10052018REA000000000000000000000001006760                          ' \
+    #    assert '34100011C2041040 207179434000140                    01111 000011111111 3BF SERVICOS DE COBRANCA LTDA' \
+    #           '                                          AV ANDROMEDA                  02000BL84 ANDAR     BARUERI ' \
+    #           '            06473000SP                  3410001300001A00000003301111 000011111111 0CLIENTE TESTE    ' \
+    #           '             5511972063805440    10052018REA000000000000000000000001006760                          ' \
     #            '  00000000000000056465100657843      000000111111111111112                      34100015        ' \
     #            ' 000003000000000001006760000000000000000000                                                      ' \
     #            '                                                                                                  ' \
     #            '                             ' in body_big_line
-    #
+
     # @staticmethod
     # def test_5_process_many_payments():
     #     payment_file = File('Itau', company)
@@ -232,28 +146,28 @@ class ItauFileTestCase(unittest.TestCase):
     #     f.write(body_big_line)
     #     f.close()
     #
-    #     assert '34100011C2041040 207179434000140                    01111 000011111111 3BF SERVICOS DE COBRANCA LTDA' \
+    #    assert '34100011C2041040 207179434000140                    01111 000011111111 3BF SERVICOS DE COBRANCA LTDA' \
     #            '                                          AV ANDROMEDA                  02000BL84 ANDAR     BARUERI' \
     #            '             06473000SP                  3410001300001A00000003301111 000011111111 0CLIENTE TESTE' \
-    #            '                 5511972063805440    10052018REA904008880000000000000000240000                      ' \
-    #            '      000000000000000                    00000011111111111111                       34100015         ' \
-    #            '000003000000000000240000000000000000000000                                                           ' \
-    #            '                                                                                                     ' \
-    #            '                     34100021C2031030 207179434000140                    ' \
-    #            '01111 000011111111 3BF SERVICOS DE COBRANCA LTDA                                          ' \
-    #            'AV ANDROMEDA                  02000BL84 ANDAR     BARUERI             06473000SP                  ' \
-    #            '3410002300001J00003394752400010067609485239700000007661190101CLIENTE TESTE                 ' \
-    #            '14052018000000001006760000000000000000000000000000000100520180000000010067600000000000000005506715023' \
-    #            '835136                                          34100025         000003000000000001006760000000000000' \
-    #            '000000                                                                                               ' \
-    #            '                                                                                      ' \
-    #            '34100031C2041040 207179434000140                    01111 000011111111 3BF SERVICOS DE COBRANCA LTDA' \
-    #            '                                          AV ANDROMEDA                  02000BL84 ANDAR     BARUERI' \
-    #            '             06473000SP                  3410003300001A00000003301111 000011111111 0CLIENTE TESTE    ' \
-    #            '             5511972063805440    10052018REA000000000000000000000001006760                           ' \
-    #            ' 00000000000000056465100657843      000000111111111111112                      34100035         ' \
-    #            '000003000000000001006760000000000000000000                                                           ' \
-    #            '                                                                                                     ' \
+    #          '                 5511972063805440    10052018REA904008880000000000000000240000                      ' \
+    #          '      000000000000000                    00000011111111111111                       34100015         ' \
+    #          '000003000000000000240000000000000000000000                                                           ' \
+    #          '                                                                                                     ' \
+    #           '                     34100021C2031030 207179434000140                    ' \
+    #           '01111 000011111111 3BF SERVICOS DE COBRANCA LTDA                                          ' \
+    #           'AV ANDROMEDA                  02000BL84 ANDAR     BARUERI             06473000SP                  ' \
+    #           '3410002300001J00003394752400010067609485239700000007661190101CLIENTE TESTE                 ' \
+    #          '14052018000000001006760000000000000000000000000000000100520180000000010067600000000000000005506715023' \
+    #          '835136                                          34100025         000003000000000001006760000000000000' \
+    #          '000000                                                                                               ' \
+    #           '                                                                                      ' \
+    #           '34100031C2041040 207179434000140                    01111 000011111111 3BF SERVICOS DE COBRANCA LTDA' \
+    #           '                                          AV ANDROMEDA                  02000BL84 ANDAR     BARUERI' \
+    #          '             06473000SP                  3410003300001A00000003301111 000011111111 0CLIENTE TESTE    ' \
+    #          '             5511972063805440    10052018REA000000000000000000000001006760                           ' \
+    #           ' 00000000000000056465100657843      000000111111111111112                      34100035         ' \
+    #          '000003000000000001006760000000000000000000                                                           ' \
+    #          '                                                                                                     ' \
     #            '                     ' in body_big_line
     #
     # @staticmethod
@@ -318,8 +232,8 @@ class ItauFileTestCase(unittest.TestCase):
     #     payment_file.generate()
     #
     #     footer_line = payment_file.footer.to_line()
-    #     assert '34199999         000003000011                                                                        ' \
-    #            '                                                                                                     ' \
+    #   assert '34199999         000003000011                                                                        ' \
+    #          '                                                                                                     ' \
     #            '                                      ' in footer_line
     #
     # @staticmethod
