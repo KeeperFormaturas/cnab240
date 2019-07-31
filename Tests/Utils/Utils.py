6963fc372056837635b13668233461626d8f523f
Guilherme <guiligan@gmail.com>
2019-07-31 14:25:21 -0300

    Separate companies and payments in utils

--- Tests/Utils/Utils.py
+++ Tests/Utils/Utils.py
@@ -1,3 +1,4 @@
+from keeper_cnab240.file import File
 import hashlib
 
 
@@ -17,3 +18,15 @@ def file_as_blockiter(afile, blocksize=65536):
         while len(block) > 0:
             yield block
             block = afile.read(blocksize)
+
+
+def payment_file_generator(bank_name, company, payments):
+    payment_file = File(bank_name, company)
+    payment_file.header.set_company_data(company)
+    payment_file.payments = []
+
+    for payment in payments:
+        payment_file.add_payment(payment)
+
+    payment_file.generate()
+    return payment_file.get_content()
