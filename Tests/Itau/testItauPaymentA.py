82cbf7762c01e7df83bc1d3774274b846ca1ff28
Guilherme <guiligan@gmail.com>
2019-07-31 14:26:03 -0300

    Update tests to reflect new structure

--- Tests/Itau/testItauPaymentA.py
+++ Tests/Itau/testItauPaymentA.py
@@ -1,9 +1,8 @@
-from .ItauUtils import company, company_j
 from keeper_cnab240.banks.Itau.payment_methods import PaymentMethods
-from keeper_cnab240.file import File
-from keeper_cnab240.payment import Payment
+from Tests.Utils.Companies import company
+from Tests.Utils.Payments import payments
+from Tests.Utils.Utils import hash_file, payment_file_generator
 import os
-import Tests.Utils as Utils
 import unittest
 
 
@@ -16,24 +15,8 @@ class TestItauPaymentA(unittest.TestCase):
 
     @staticmethod
     def test_process_one_paymentA():
-        payment_file = File('Itau', company)
-        payment_file.header.set_company_data(company)
-        payment_file.payments = []
-
-        payment = Payment(type=PaymentMethods.TED,
-                          favored_name=company_j.name,
-                          favored_bank=company_j.bank_account.bank_code,
-                          agency=company.bank_account.agency,
-                          account=company.bank_account.account,
-                          account_digit=company.bank_account.digit,
-                          your_number='n1U0fXg4Tl2RLotU0YQ8',
-                          pay_date='01012019',
-                          payment_amount=2400.00,
-                          favored_document_number=company_j.document)
-        payment_file.add_payment(payment)
-        payment_file.generate()
-        body_big_line = payment_file.get_content()
-
+        _payments = payments(PaymentMethods.TED)
+        body_big_line = payment_file_generator('Itau', company, [_payments['A1']])
         root_path = os.path.dirname(os.path.abspath(__file__))
         body_big_file_path = os.path.join(root_path, 'body_big_file_segment_a.rem')
 
@@ -41,7 +24,7 @@ class TestItauPaymentA(unittest.TestCase):
         f.write(body_big_line)
         f.close()
 
-        file_hash = Utils.hash_file(body_big_file_path, True)
+        file_hash = hash_file(body_big_file_path, True)
         os.remove(body_big_file_path)
 
         assert 'b31957cda8950105f0e659b9ed70ecc01ef1aee253e59d56d3596d6ffc6aed93' == file_hash
